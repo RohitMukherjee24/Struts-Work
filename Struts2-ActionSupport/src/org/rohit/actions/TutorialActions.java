b6fda30fb4a09c3785d3a34c71f8e2db23c9c8b8
RohitMukherjee24 <rohitjee24@gmail.com>
2017-01-25 15:54:56 -0500

    ActionSupport class useage

---
+++ Struts2-ActionSupport/src/org/rohit/actions/TutorialActions.java
@@ -0,0 +1,39 @@
+package org.rohit.actions;
+
+import org.rohit.service.TutorialFinderService;
+
+/**
+ * Created by rohit on 1/24/2017.
+ */
+public class TutorialActions {
+
+    private String bestTutorialSight;
+    private  String language;
+
+    public String execute(){
+        TutorialFinderService tutorialFinderService = new TutorialFinderService();
+//        System.out.println(getLanguage());
+//        System.out.println(tutorialFinderService.getBestTutorialSite());
+        setBestTutorialSight(tutorialFinderService.getBestTutorialSite(getLanguage()));
+        System.out.println("Execute Method Runs!!!");
+        return "success";
+        //return "failure";
+        //return "searchForm";
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getBestTutorialSight() {
+        return bestTutorialSight;
+    }
+
+    public void setBestTutorialSight(String bestTutorialSight) {
+        this.bestTutorialSight = bestTutorialSight;
+    }
+}

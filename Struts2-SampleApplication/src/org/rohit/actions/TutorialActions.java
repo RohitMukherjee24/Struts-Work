c951e818f7b6fb624865ec6d16233336364d8633
RohitMukherjee24 <rohitjee24@gmail.com>
2017-01-24 20:02:48 -0500

    Adding Struts Sample Application

---
+++ Struts2-SampleApplication/src/org/rohit/actions/TutorialActions.java
@@ -0,0 +1,12 @@
+package org.rohit.actions;
+
+/**
+ * Created by rohit on 1/24/2017.
+ */
+public class TutorialActions {
+    public String execute(){
+        System.out.println("Execute Method Runs!!!");
+        //return "success";
+        return "failure";
+    }
+}
